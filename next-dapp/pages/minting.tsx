1425bfc448e3f2093fc1b8be29bd4fda98230311
George Safta <geo.safta@gmail.com>
2022-03-29 22:45:59 +0300

    Add fonts to minting page

--- next-dapp/pages/minting.tsx
+++ next-dapp/pages/minting.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import Footer from "../components/Footer";
+import IndexPage from "../components/Head";
 import Mint from "../components/Minting";
 
 const Minting: NextPage = () => {
   return (
     <div>
+      <IndexPage />
       <Mint />
       <Footer page="mint" />
     </div>
