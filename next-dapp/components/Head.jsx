8616c3ed89af691cc5475402d03b0f3c3d0688c1
George Safta <geo.safta@gmail.com>
2022-03-31 10:06:58 +0300

    Add google analytics

--- next-dapp/components/Head.jsx
+++ next-dapp/components/Head.jsx
@@ -7,7 +7,10 @@ export default function IndexPage() {
         <title>Zyggo Distrikt</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
+          rel="stylesheet"
+        />
       </Head>
     </div>
   )
