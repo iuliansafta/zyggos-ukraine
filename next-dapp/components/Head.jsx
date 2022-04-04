8f20840f0d4ad28f821a4a17dad92a3365a8cd9f
Iulian <iulian.safta@gmail.com>
2022-04-04 23:22:03 +0200

    fake description

--- next-dapp/components/Head.jsx
+++ next-dapp/components/Head.jsx
@@ -7,7 +7,10 @@ export default function IndexPage() {
         <title>Zyggo Distrikt</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <meta name="description" content="We rise by lifting others." />
+        <meta
+          name="description"
+          content="We are the skaters of the internet."
+        />
         <meta property="og:title" content="NFTS FOR GOOD" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://zyggodistrikt.com/" />
@@ -15,7 +18,10 @@ export default function IndexPage() {
           property="og:image"
           content="https://www.zyggodistrikt.com/images/twitterimage.jpg"
         />
-        <meta property="og:description" content="We rise by lifting others." />
+        <meta
+          property="og:description"
+          content="We are the skaters of the internet."
+        />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@ZyggoDistrikt" />
         <meta name="twitter:creator" content="@ZyggoDistrikt" />
@@ -23,7 +29,10 @@ export default function IndexPage() {
           name="twitter:image"
           content="https://www.zyggodistrikt.com/images/twitterimage.jpg"
         />
-        <meta name="twitter:description" content="We rise by lifting others." />
+        <meta
+          name="twitter:description"
+          content="We are the skaters of the internet."
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
           rel="stylesheet"
