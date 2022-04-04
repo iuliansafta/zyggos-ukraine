6b7697cbeb36ec7652125afa22568d9079a718c0
Iulian <iulian.safta@gmail.com>
2022-04-04 23:46:57 +0200

    fixes preload fonts

--- next-dapp/components/Head.jsx
+++ next-dapp/components/Head.jsx
@@ -1,36 +1,47 @@
 import { NextSeo } from "next-seo";
+import Head from "next/head";
 
 export default function IndexPage() {
   return (
-    <NextSeo
-      title="Zyggo Distrikt"
-      description="We rise by lifting others."
-      canonical="https://www.canonical.ie/"
-      openGraph={{
-        url: "https://zyggodistrikt.com/",
-        title: "NFTS FOR GOOD",
-        description: "We rise by lifting others.",
-        images: [
-          {
-            url: "https://www.zyggodistrikt.com/images/twitterimage.jpg",
-            width: 2048,
-            height: 1264,
-            alt: "NFTS FOR GOOD",
-            type: "image/jpeg",
-          },
-        ],
-      }}
-      twitter={{
-        cardType: "summary",
-        site: "@ZyggoDistrikt",
-        handle: "@ZyggoDistrikt",
-      }}
-    />
+    <>
+      <NextSeo
+        title="Zyggo Distrikt"
+        description="We rise by lifting others."
+        canonical="https://www.canonical.ie/"
+        openGraph={{
+          url: "https://zyggodistrikt.com/",
+          title: "NFTS FOR GOOD",
+          description: "We rise by lifting others.",
+          images: [
+            {
+              url: "https://www.zyggodistrikt.com/images/twitterimage.jpg",
+              width: 2048,
+              height: 1264,
+              alt: "NFTS FOR GOOD",
+              type: "image/jpeg",
+            },
+          ],
+        }}
+        twitter={{
+          cardType: "summary",
+          site: "@ZyggoDistrikt",
+          handle: "@ZyggoDistrikt",
+        }}
+      />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+    </>
     // <div>
     //   <Head>
     //     <title>Zyggo Distrikt</title>
-    //     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    //     <link rel="preconnect" href="https://fonts.gstatic.com" />
+    // <link rel="preconnect" href="https://fonts.googleapis.com" />
+    // <link rel="preconnect" href="https://fonts.gstatic.com" />
     //     <meta name="description" content="We rise by lifting others." />
     //     <meta property="og:title" content="NFTS FOR GOOD" />
     //     <meta property="og:type" content="website" />
@@ -48,10 +59,10 @@ export default function IndexPage() {
     //       content="https://www.zyggodistrikt.com/images/twitterimage.jpg"
     //     />
     //     <meta name="twitter:description" content="We rise by lifting others." />
-    //     <link
-    //       href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
-    //       rel="stylesheet"
-    //     />
+    // <link
+    //   href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
+    //   rel="stylesheet"
+    // />
     //   </Head>
     // </div>
   );
