05747670734b1364fc97a6dfb238a9fabb8a87c8
Iulian <iulian.safta@gmail.com>
2022-04-04 23:42:38 +0200

    changed seo headers

--- next-dapp/components/Head.jsx
+++ next-dapp/components/Head.jsx
@@ -1,43 +1,58 @@
-import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 export default function IndexPage() {
   return (
-    <div>
-      <Head>
-        <title>Zyggo Distrikt</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <meta
-          name="description"
-          content="We are the skaters of the internet."
-        />
-        <meta property="og:title" content="NFTS FOR GOOD" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://zyggodistrikt.com/" />
-        <meta
-          property="og:image"
-          content="https://www.zyggodistrikt.com/images/twitterimage.jpg"
-        />
-        <meta
-          property="og:description"
-          content="We are the skaters of the internet."
-        />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@ZyggoDistrikt" />
-        <meta name="twitter:creator" content="@ZyggoDistrikt" />
-        <meta
-          name="twitter:image"
-          content="https://www.zyggodistrikt.com/images/twitterimage.jpg"
-        />
-        <meta
-          name="twitter:description"
-          content="We are the skaters of the internet."
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-    </div>
+    <NextSeo
+      title="Zyggo Distrikt"
+      description="We rise by lifting others."
+      canonical="https://www.canonical.ie/"
+      openGraph={{
+        url: "https://zyggodistrikt.com/",
+        title: "NFTS FOR GOOD",
+        description: "We rise by lifting others.",
+        images: [
+          {
+            url: "https://www.zyggodistrikt.com/images/twitterimage.jpg",
+            width: 2048,
+            height: 1264,
+            alt: "NFTS FOR GOOD",
+            type: "image/jpeg",
+          },
+        ],
+      }}
+      twitter={{
+        cardType: "summary",
+        site: "@ZyggoDistrikt",
+        handle: "@ZyggoDistrikt",
+      }}
+    />
+    // <div>
+    //   <Head>
+    //     <title>Zyggo Distrikt</title>
+    //     <link rel="preconnect" href="https://fonts.googleapis.com" />
+    //     <link rel="preconnect" href="https://fonts.gstatic.com" />
+    //     <meta name="description" content="We rise by lifting others." />
+    //     <meta property="og:title" content="NFTS FOR GOOD" />
+    //     <meta property="og:type" content="website" />
+    //     <meta property="og:url" content="https://zyggodistrikt.com/" />
+    //     <meta
+    //       property="og:image"
+    //       content="https://www.zyggodistrikt.com/images/twitterimage.jpg"
+    //     />
+    //     <meta property="og:description" content="We rise by lifting others." />
+    //     <meta name="twitter:card" content="summary" />
+    //     <meta name="twitter:site" content="@ZyggoDistrikt" />
+    //     <meta name="twitter:creator" content="@ZyggoDistrikt" />
+    //     <meta
+    //       name="twitter:image"
+    //       content="https://www.zyggodistrikt.com/images/twitterimage.jpg"
+    //     />
+    //     <meta name="twitter:description" content="We rise by lifting others." />
+    //     <link
+    //       href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
+    //       rel="stylesheet"
+    //     />
+    //   </Head>
+    // </div>
   );
 }
