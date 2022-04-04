420b82ecd44a14f0b3b74a6159ef0ba84055e321
Iulian <iulian.safta@gmail.com>
2022-04-04 22:53:01 +0200

    Added GTM instead of analytics

--- next-dapp/pages/_app.tsx
+++ next-dapp/pages/_app.tsx
@@ -1,26 +1,28 @@
-import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import Script from 'next/script'
-import { useEffect } from 'react'
-import * as gtag from '../lib/gtag'
-import '../styles/globals.scss'
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+import TagManager from "react-gtm-module";
+import "../styles/globals.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter()
+  // const router = useRouter();
   useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+    // const handleRouteChange = (url: any) => {
+    //   gtag.pageview(url)
+    // }
+    // router.events.on('routeChangeComplete', handleRouteChange)
+    // return () => {
+    //   router.events.off('routeChangeComplete', handleRouteChange)
+    // }
+    TagManager.initialize({ gtmId: "GTM-WTH9H2M" });
+  }, []);
 
   return (
     <>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
+      {/* <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
@@ -37,10 +39,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
             });
           `,
         }}
-      />
+      /> */}
       <Component {...pageProps} />
     </>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
